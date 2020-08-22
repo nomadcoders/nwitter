32c97c45797ea3ea6ed7e1003ade2cbaa7d49a33
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-22 14:26:01 +0900

    Updates

---
+++ precache-manifest.31a0ab967bf4718844484979ee3186b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38708e3034b1d4a83278667b7dd8a975",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "eb415937dd46e5df4a8e",
+    "url": "/nwitter/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "60fc5522298b6ecf7cd4",
+    "url": "/nwitter/static/js/2.3e7859c2.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.3e7859c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb415937dd46e5df4a8e",
+    "url": "/nwitter/static/js/main.f69015b4.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);

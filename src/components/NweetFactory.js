1e782ed942b80314cc7762abdf5ad894e2dc9643
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-22 14:46:28 +0900

    input box sizing

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -8,10 +8,10 @@ const NweetFactory = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [attachment, setAttachment] = useState("");
   const onSubmit = async (event) => {
+    event.preventDefault();
     if (nweet === "") {
       return;
     }
-    event.preventDefault();
     let attachmentUrl = "";
     if (attachment !== "") {
       const attachmentRef = storageService

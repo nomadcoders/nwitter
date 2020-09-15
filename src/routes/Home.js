e751fcd10ba4e9385730539373b2023d1844ebf0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-09-15 23:58:00 +0900

    order + title

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,13 +6,16 @@ import NweetFactory from "components/NweetFactory";
 const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
   useEffect(() => {
-    dbService.collection("nweets").onSnapshot((snapshot) => {
-      const nweetArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNweets(nweetArray);
-    });
+    dbService
+      .collection("nweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const nweetArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNweets(nweetArray);
+      });
   }, []);
   return (
     <div className="container">

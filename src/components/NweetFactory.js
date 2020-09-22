b1b78d82c347bb768929bc928114a937c54ef421
kennycha <59906546+kennycha@users.noreply.github.com>
2020-09-23 01:32:38 +0900

    Update NweetFactory.js

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -48,7 +48,9 @@ const NweetFactory = ({ userObj }) => {
       } = finishedEvent;
       setAttachment(result);
     };
-    reader.readAsDataURL(theFile);
+    if (Boolean(theFile)) {
+      reader.readAsDataURL(theFile);
+    }
   };
   const onClearAttachment = () => setAttachment("");
   return (

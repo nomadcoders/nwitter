777d1ce008e8d911a58421acc0e319b21d0188d0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-12-29 12:28:27 +0900

    compat upgrade

--- src/fbase.js
+++ src/fbase.js
@@ -1,7 +1,7 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
